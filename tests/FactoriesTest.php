0b7b3cca1ece8cecba4c30b3a56bf7ccdff82752
Anton Smirnov <sandfox+git@sandfox.me>
2024-02-26 05:47:40 +0200

    Test Gregorian

--- tests/FactoriesTest.php
+++ tests/FactoriesTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Arokettu\Date\Tests;
 
 use Arokettu\Date\Date;
+use Arokettu\Date\Month;
 use PHPUnit\Framework\TestCase;
 
 class FactoriesTest extends TestCase
@@ -42,5 +43,39 @@ class FactoriesTest extends TestCase
         self::assertEquals($sysDate2, (string)$date2);
 
         self::assertEquals($date2, $date1->add(1));
+    }
+
+    public function testGregorianDate(): void
+    {
+        $date1 = Date::create(2024, 2, 25);
+        $date2 = Date::create(2024, Month::February, 25); // use enum for a month
+
+        self::assertEquals($date1, $date2);
+        self::assertEquals('2024-02-25', (string)$date1);
+    }
+
+    public function testGregorianDateNegJD(): void
+    {
+        $date1 = Date::create(-5120, 5, 13);
+        $date2 = Date::create(-5120, Month::May, 13); // use enum for a month
+
+        self::assertEquals($date1, $date2);
+        self::assertEquals('-5120-05-13', (string)$date1);
+    }
+
+    public function testGregorianDateWrongMonth(): void
+    {
+        $this->expectException(\DomainException::class);
+        $this->expectExceptionMessage('Month must be an instance of Month or an integer 1-12');
+
+        Date::create(2000, 13, 13);
+    }
+
+    public function testGregorianDateWrongDay(): void
+    {
+        $this->expectException(\DomainException::class);
+        $this->expectExceptionMessage('For year 2000 month 11, day must be in range 1-30');
+
+        Date::create(2000, 11, 33);
     }
 }
--- tests/GettersTest.php
+++ tests/GettersTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Arokettu\Date\Tests;
 
 use Arokettu\Date\Date;
+use Arokettu\Date\Month;
 use Arokettu\Date\WeekDay;
 use PHPUnit\Framework\TestCase;
 
@@ -14,5 +15,39 @@ class GettersTest extends TestCase
     {
         $date = new Date(2460366);
         self::assertEquals(WeekDay::Sunday, $date->getWeekDay());
+
+        $date = new Date(-2460366);
+        self::assertEquals(WeekDay::Tuesday, $date->getWeekDay());
+    }
+
+    public function testJulianDay(): void
+    {
+        $date = new Date(2460366);
+        self::assertEquals(2460366, $date->getJulianDay());
+
+        $date = new Date(-2460366);
+        self::assertEquals(-2460366, $date->getJulianDay());
+    }
+
+    public function testGregorianDate(): void
+    {
+        $date = new Date(2460366);
+
+        self::assertEquals('2024-02-25', $date->toString());
+        self::assertEquals(2024, $date->getYear());
+        self::assertEquals(Month::February, $date->getMonth());
+        self::assertEquals(2, $date->getMonthNumber());
+        self::assertEquals(25, $date->getDay());
+    }
+
+    public function testGregorianDateNegJD(): void
+    {
+        $date = new Date(-2460366);
+
+        self::assertEquals('-11449-08-24', $date->toString());
+        self::assertEquals(-11449, $date->getYear());
+        self::assertEquals(Month::August, $date->getMonth());
+        self::assertEquals(8, $date->getMonthNumber());
+        self::assertEquals(24, $date->getDay());
     }
 }
