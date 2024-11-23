8c2eea24c0426f1f0a6795a035223bd047486756
Anton Smirnov <sandfox+git@sandfox.me>
2024-11-23 04:38:53 +0200

    Sort constants by date

--- src/CivilCalendar.php
+++ src/CivilCalendar.php
@@ -11,19 +11,19 @@ use UnexpectedValueException;
 
 final readonly class CivilCalendar
 {
-    public const MIN = 1794168; // 200-03-01
+    public const MIN        = 1794168; // 200-03-01
 
-    public const ITALY = 2299161; // 1582-10-15
-    public const BRITAIN = 2361222; // 1752-09-14
-    public const RUSSIA = 2421639; // 1918-02-14
-    public const ESTONIA = 2421654; // 1918-03-01
+    public const ITALY      = 2299161; // 1582-10-15
+    public const HUNGARY    = 2301004; // 1587-11-01
+    public const DENMARK    = 2342032; // 1700-03-01
+    public const BRITAIN    = 2361222; // 1752-09-14
+    public const SWEDEN     = 2361390; // 1753-03-01 // disregarding the madness of 1700-1712
+    public const ALBANIA    = 2419735; // 1912-11-28
+    public const BULGARIA   = 2420968; // 1916-04-14
+    public const RUSSIA     = 2421639; // 1918-02-14
+    public const ESTONIA    = 2421654; // 1918-03-01
     public const YUGOSLAVIA = 2421987; // 1919-01-28
-    public const SWEDEN = 2361390; // 1753-03-01 // disregarding the madness of 1700-1712
-    public const ALBANIA = 2419735; // 1912-11-28
-    public const BULGARIA = 2420968; // 1916-04-14
-    public const DENMARK = 2342032; // 1700-03-01
-    public const GREECE = 2423480; // 1923-03-01
-    public const HUNGARY = 2301004; // 1587-11-01
+    public const GREECE     = 2423480; // 1923-03-01
 
     public function __construct(
         public Date $switchDay
